6c562292dccbeb3b4e40f48a2136748369f2b4c2
cd155 <cd155@163.com>
2023-08-02 11:40:01 -0400

    create college model

--- buildDjango/chefsTable/demoapp/models.py
+++ buildDjango/chefsTable/demoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class college(models.Model):
+    CollegeID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
