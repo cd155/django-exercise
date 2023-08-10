a533a8ed5a1643fd873a36938e82ff358ddae765
cd155 <cd155@163.com>
2023-08-09 20:59:49 -0400

    add test case

--- buildDjango/chefsTable/demoapp/tests.py
+++ buildDjango/chefsTable/demoapp/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Logger
+from datetime import datetime
+from django.utils import timezone
 
 # Create your tests here.
+
+
+class LoggerTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Logger.objects.create(
+            first_name="Ted",
+            last_name="Li",
+            time_log=timezone.now(),
+            date_log=timezone.now()
+        )
+
+    def test_logger_types(self):
+        tedli = Logger.objects.get(
+            first_name="Ted",
+            last_name="Li",
+        )
+        self.assertIsInstance(tedli.date_log, datetime)
