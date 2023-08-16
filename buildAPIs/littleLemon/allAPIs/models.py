96125104bb596c819d823b4afbd91d27918d91b6
cd155 <cd155@163.com>
2023-08-15 20:52:14 -0400

    create bookl model

--- buildAPIs/littleLemon/allAPIs/models.py
+++ buildAPIs/littleLemon/allAPIs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'title: {self.title}, author: {self.author}'
+
+    class Meta:
+        # create price indexes
+        indexes = models.Index(fields=['price']),
