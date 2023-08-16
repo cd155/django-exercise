8dfd2c68e4bfb5437f45fc44bf3540c5d31a5733
cd155 <cd155@163.com>
2023-08-16 15:05:32 -0400

    enable django debug tool bar

--- buildAPIs/shell.nix
+++ buildAPIs/shell.nix
@@ -3,6 +3,7 @@ let
   my-python-packages = ps: with ps; [
     django
     djangorestframework
+    django-debug-toolbar
     # other python packages
   ];
   my-python = pkgs.python3.withPackages my-python-packages;
