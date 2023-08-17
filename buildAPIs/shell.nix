c507f73d4df301aef046a750760c78fca3e5b60e
cd155 <cd155@163.com>
2023-08-17 18:31:58 -0400

    add djangorestframework-xml in nix-shell

--- buildAPIs/shell.nix
+++ buildAPIs/shell.nix
@@ -5,6 +5,23 @@ let
     djangorestframework
     django-debug-toolbar
     # other python packages
+
+    # Install package not available in Nixpkgs
+    (
+      buildPythonPackage rec {
+        pname = "djangorestframework-xml";
+        version = "2.0.0";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "35f6c811d0ab8c8466b26db234e16a2ed32d76381715257aebf4c7be2c202ca1";
+        };
+        doCheck = false;
+        propagatedBuildInputs = [
+          # Specify dependencies
+          pkgs.python3Packages.defusedxml
+        ];
+      }
+    )
   ];
   my-python = pkgs.python3.withPackages my-python-packages;
 in my-python.env
