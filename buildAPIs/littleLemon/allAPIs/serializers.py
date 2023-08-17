5157bd7508382bca65e83f5c2b66440defb2f9ae
cd155 <cd155@163.com>
2023-08-17 18:32:58 -0400

    update BookSerializer

--- buildAPIs/littleLemon/allAPIs/serializers.py
+++ buildAPIs/littleLemon/allAPIs/serializers.py
@@ -5,4 +5,8 @@ from rest_framework import serializers
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'price']
+        fields = ['id', 'title', 'author', 'price', 'inventory']
+        extra_kwargs = {
+            'price': {'min_value': 2},
+            'inventory': {'min_value': 0},
+        }
