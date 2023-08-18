f7233e99fbe0dd8a869f7d7ac7649894e8c1a3ee
cd155 <cd155@163.com>
2023-08-18 01:07:43 -0400

    change field name, assign method to calculate a field value

--- buildAPIs/littleLemon/allAPIs/serializers.py
+++ buildAPIs/littleLemon/allAPIs/serializers.py
@@ -1,19 +1,37 @@
 from .models import Book, Category
 from rest_framework import serializers
-
-
-class BookSerializer(serializers.ModelSerializer):
-    category = serializers.StringRelatedField()
-    class Meta:
-        model = Book
-        fields = ['id', 'title', 'author', 'price', 'inventory', 'category']
-        extra_kwargs = {
-            'price': {'min_value': 2},
-            'inventory': {'min_value': 0},
-        }
+from decimal import Decimal
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class BookSerializer(serializers.ModelSerializer):
+    # convert category object to strin
+    # category = serializers.StringRelatedField()
+
+    # set full category
+    category = CategorySerializer()
+
+    # assign inventory field to stock
+    stock = serializers.IntegerField(source='inventory')
+
+    # assign price_after_tax with a method
+    price_after_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax'
+    )
+
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'price',
+                  'stock', 'category', 'price_after_tax']
+        extra_kwargs = {
+            'price': {'min_value': 2},
+            'inventory': {'min_value': 0},
+        }
+
+    def calculate_tax(self, product: Book):
+        return product.price * Decimal(1.13)
