3306576ab726b3014ca4ee599e0e8f9455edf75c
cd155 <cd155@163.com>
2023-08-18 15:17:16 -0400

    uncomment categories

--- buildAPIs/littleLemon/allAPIs/urls.py
+++ buildAPIs/littleLemon/allAPIs/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('books/<str:title>', views.SingleBookView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
-    # path('categories', views.CategoriesView.as_view()),
+    path('categories', views.CategoriesView.as_view()),
     path('categories/<int:pk>', views.SingleCategoryView.as_view(), name='category-detail'),
 
     ## for function views
