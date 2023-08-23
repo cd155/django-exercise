a32f3e4441d49f2975985427cedf8f1bea8bfeb1
cd155 <cd155@163.com>
2023-08-23 12:20:17 -0400

    add throttling in settings

--- buildAPIs/littleLemon/littleLemon/settings.py
+++ buildAPIs/littleLemon/littleLemon/settings.py
@@ -141,7 +141,16 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 
+    'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute',
+        'user': '5/minute',
+        'three': '3/minute',
+    }
 }
