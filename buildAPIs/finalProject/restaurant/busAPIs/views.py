d36f1ef5b9757e3a7e9d4764c325f9c2b039a285
cd155 <cd155@163.com>
2023-08-25 13:01:59 -0400

    create home view

--- buildAPIs/finalProject/restaurant/busAPIs/views.py
+++ buildAPIs/finalProject/restaurant/busAPIs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def home(request):
+    return Response('good', status=status.HTTP_200_OK)
