6dca82a173ac902e592958d2053721c9ae6316d9
cd155 <cd155@163.com>
2023-08-25 14:03:00 -0400

    add djoser urls

--- buildAPIs/finalProject/restaurant/busAPIs/urls.py
+++ buildAPIs/finalProject/restaurant/busAPIs/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'busAPIs'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
 ]
