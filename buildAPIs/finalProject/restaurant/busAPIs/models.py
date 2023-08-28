a78025b8ee5c15a65894e07ec4a467f901307ab9
cd155 <cd155@163.com>
2023-08-28 17:51:01 -0400

    display category title

--- buildAPIs/finalProject/restaurant/busAPIs/models.py
+++ buildAPIs/finalProject/restaurant/busAPIs/models.py
@@ -6,6 +6,8 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
