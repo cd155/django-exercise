5afe78a30f159e1f6e559ba17f90f6a3f0152102
cd155 <cd155@163.com>
2023-08-29 14:51:55 -0400

    add UserSerializer

--- buildAPIs/finalProject/restaurant/busAPIs/serializers.py
+++ buildAPIs/finalProject/restaurant/busAPIs/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import Category, MenuItem
-
+from django.contrib.auth.models import User
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +13,10 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
