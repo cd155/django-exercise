d4095e3c1de4fed1215b758cccb72cc4533d8781
cd155 <cd155@163.com>
2023-08-29 15:03:35 -0400

    change functional view to class view

--- buildAPIs/finalProject/restaurant/busAPIs/views.py
+++ buildAPIs/finalProject/restaurant/busAPIs/views.py
@@ -8,9 +8,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 
 from .models import Category, MenuItem, Cart, Order, OrderItem
-from .serializers import CategorySerializer,\
-                         MenuItemSerializer,\
-                         UserSerializer
+from .serializers import CategorySerializer, MenuItemSerializer,\
+    UserSerializer
 
 
 @api_view(['GET', 'POST'])
@@ -40,7 +39,7 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    
+
     def put(self, request, pk):
         if request.user.groups.filter(name='Manager').exists():
             return super().put(request)
@@ -52,19 +51,27 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
             return super().patch(request)
         else:
             return Response(status=status.HTTP_403_FORBIDDEN)
-    
+
     def delete(self, request, pk):
         if request.user.groups.filter(name='Manager').exists():
             return super().delete(request)
         else:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
-@api_view(['GET'])
+
 @permission_classes([IsAuthenticated])
-def managers(request):
-    if request.user.groups.filter(name='Manager').exists():
-        users = User.objects.filter(groups__name='Manager')
-        serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, status=201, safe=False)
-    else:
-        return Response(status=status.HTTP_403_FORBIDDEN)
+class ManagersView(generics.ListCreateAPIView):
+    queryset = User.objects.filter(groups__name='Manager')
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        if request.user.groups.filter(name='Manager').exists():
+            return super().get(request)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def post(self, request):
+        if request.user.groups.filter(name='Manager').exists():
+            return super().post(request)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
