6677b58831efe9a1e2de8308a82a05740f9144ae
cd155 <cd155@163.com>
2023-08-28 23:43:07 -0400

    add IsAuthenticated, and override post method

--- buildAPIs/finalProject/restaurant/busAPIs/views.py
+++ buildAPIs/finalProject/restaurant/busAPIs/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth.models import User, Group
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 from .models import Category, MenuItem, Cart, Order, OrderItem
 from .serializers import CategorySerializer, MenuItemSerializer
@@ -17,7 +20,15 @@ class CategoriesView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
 
 
+@permission_classes([IsAuthenticated])
 class MenuItemsView(generics.ListCreateAPIView):
     # more efficient for relation in two models
     queryset = MenuItem.objects.select_related('category').all()
     serializer_class = MenuItemSerializer
+
+    def post (self, request):
+        if request.user.groups.filter(name='Manager').exists():
+            return super().post(request)
+        else:
+            return Response('Only managers can edit menu items.', status=status.HTTP_403_FORBIDDEN)
+        
