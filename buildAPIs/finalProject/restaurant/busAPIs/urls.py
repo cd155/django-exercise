19f3a9a7fdb5061c4c606c97f80652cce6afc2e3
cd155 <cd155@163.com>
2023-08-30 16:11:37 -0400

    add delivery and cart url

--- buildAPIs/finalProject/restaurant/busAPIs/urls.py
+++ buildAPIs/finalProject/restaurant/busAPIs/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('categories', views.CategoriesView.as_view()),
     path('groups/manager/users', views.ManagersView.as_view()),
     path('groups/manager/users/<int:pk>', views.SingleManagerView.as_view()),
+    path('groups/delivery-crew/users', views.DeliveryCrewView.as_view()),
+    path('groups/delivery-crew/users/<int:pk>', views.SingleDeliveryCrewView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
 ]
